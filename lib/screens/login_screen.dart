966d5ff4eef57c574908b4a2b70fe6f8347d2244
John <JohnPhamDeveloper@hotmail.com>
2019-07-16 11:18:34 -0700

    init

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
