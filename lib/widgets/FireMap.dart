ee8d4265b55c08352fe57e8a1b5db0c6c4e83dc5
John <JohnPhamDeveloper@hotmail.com>
2019-07-29 15:41:40 -0700

    Added green dot for user locations

--- lib/widgets/FireMap.dart
+++ lib/widgets/FireMap.dart
@@ -6,6 +6,23 @@ import 'package:rxdart/rxdart.dart';
 import 'package:location/location.dart';
 import 'dart:async';
 
+/// Structure:
+/// 1) _startQuery()
+/// Gets user location and center point and subscribe to radius which
+/// user can change with the slider. Then go to database and only find
+/// documents around that radius. Calls _updateMarker() everytime radius changes.
+///
+/// 2) _updateMarkers()
+/// Clear all markers from screen. Then go through each document around
+/// the radius of the user and create a marker using the documents position.
+/// Add that marker to the "markers" state and it'll display onto map
+///
+/// 3) _updateQuery()
+/// Called on the slider's onChange.
+/// Whenever the slider changes, it will setState the radius variable.
+/// As a result, radius.switchMap will be called again and find all
+/// documents around that new radius.
+
 class FireMap extends StatefulWidget {
   @override
   _FireMapState createState() => _FireMapState();
@@ -25,52 +42,59 @@ class _FireMapState extends State<FireMap> {
   BehaviorSubject<double> radius = BehaviorSubject<double>.seeded(1.0);
   Stream<Query> query;
 
+  @override
+  void initState() {
+    super.initState();
+    _initOtherUserIcon();
+    _startQuery();
+  }
+
   _startQuery() async {
+    print("Starting query...");
     var pos = await location.getLocation();
     double lat = pos.latitude;
     double lng = pos.longitude;
 
     var ref = firestore.collection('locations');
+    print("Getting reference to location in firestore... $ref");
+
     GeoFirePoint center = geo.point(latitude: lat, longitude: lng);
 
     // whenever radius changes, go to our database and find all
     // documents that are within the radius using the user's center position
     subscription = radius.switchMap((rad) {
+      print("Mapping through ${rad / 1.609} miles");
       return geo.collection(collectionRef: ref).within(
             center: center,
-            radius: rad,
+            radius: rad / 1.609, // Convert KM to MILES
             field: 'position',
             strictMode: true,
           );
-    }).listen(_updateMarkers);
+    }).listen(_updateMarkers); // pass in documentSnapshot
   }
 
-  // Update query based on slider value
-  _updateQuery(double value) {
-    setState(() {
-      radius.add(value);
-    });
-  }
-
-  // All documents within the radius of the users will be displayed
-  // on the user's map
   void _updateMarkers(List<DocumentSnapshot> documentList) {
+    print("Updating markers on screen...");
     print(documentList);
 
     markers.clear();
 
+    // Go through every document in our locations collection
     documentList.forEach((snapshot) {
+      // Get position of that document
       GeoPoint pos = snapshot.data['position']['geopoint'];
       double distance = snapshot.data['distance'];
       final MarkerId markerId = MarkerId(UniqueKey().toString());
 
+      // Create marker at that position
       var marker = Marker(
         markerId: markerId,
         position: LatLng(pos.latitude, pos.longitude),
         icon: otherUserIconOnMap,
+        //icon: BitmapDescriptor.defaultMarker,
         infoWindow: InfoWindow(
           title: "Magic Marker",
-          snippet: "$distance test",
+          snippet: "$distance km",
         ),
       );
 
@@ -78,7 +102,14 @@ class _FireMapState extends State<FireMap> {
     });
   }
 
-  // Why does this return?
+  // Update query based on slider value
+  _updateQuery(double value) {
+    print("Updating query...");
+    setState(() {
+      radius.add(value);
+    });
+  }
+
   Future<DocumentReference> _addGeoPoint() async {
     // Get current user position
     var pos = await location.getLocation();
@@ -91,6 +122,17 @@ class _FireMapState extends State<FireMap> {
     return firestore.collection("locations").add({
       'position': point.data,
       'name': "hello!",
+    });
+  }
+
+  Future<DocumentReference> _addGeoPointAt(double lat, double long) async {
+    // Create a geopoint that will be stored in firebase
+    GeoFirePoint point = geo.point(latitude: lat, longitude: long);
+
+    // Add user current position to firebase
+    return firestore.collection("locations").add({
+      'position': point.data,
+      'name': "hello!NEW",
     });
   }
 
@@ -133,14 +175,15 @@ class _FireMapState extends State<FireMap> {
   }
 
   _initOtherUserIcon() async {
-    otherUserIconOnMap = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(size: Size(20, 20)), "assets/greendot.png");
-  }
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(size: Size(0.0000000001, 0.000000001)),
+            'assets/greendot.png')
+        .then((onValue) {
+      otherUserIconOnMap = onValue;
+    });
 
-  @override
-  void initState() {
-    super.initState();
-    _startQuery();
+//    otherUserIconOnMap = await BitmapDescriptor.fromAssetImage(
+//        ImageConfiguration(size: Size(0.005, 0.005)), "assets/greendot.png");
   }
 
   @override
@@ -173,7 +216,7 @@ class _FireMapState extends State<FireMap> {
             max: 5,
             divisions: 5,
             value: radius.value,
-            label: 'Radius ${radius.value}km',
+            label: 'Radius ${radius.value}km?',
             activeColor: Colors.cyan[300],
             inactiveColor: Colors.cyan[300].withOpacity(0.2),
             onChanged: _updateQuery,
@@ -183,7 +226,9 @@ class _FireMapState extends State<FireMap> {
           bottom: 20,
           left: 20,
           child: InkWell(
-            onTap: _addGeoPoint,
+            onTap: () {
+              _addGeoPointAt(37.315616, -121.831424);
+            },
             child: Icon(Icons.my_location, size: 30),
           ),
         ),
