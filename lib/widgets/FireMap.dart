3da2a4031431f48e165778ee1c58ebc2439b8720
John <JohnPhamDeveloper@hotmail.com>
2019-08-01 00:33:34 -0700

    added setstate to clearmarkers when selfie mode is off

--- lib/widgets/FireMap.dart
+++ lib/widgets/FireMap.dart
@@ -89,7 +89,9 @@ class _FireMapState extends State<FireMap> {
         print("------------CLEARING MARKERS DUE TO NOT BEING IN SELFIE MODE-------------");
         // TODO this should also change but we should have different icons for business and hangouts.
         // TODO otherwise selfie, business, and hangout can all run concurrently. Though they neeed a sbuscription (too much reads)
-        markers.clear();
+        setState(() {
+          markers.clear();
+        });
       }
     });
 
