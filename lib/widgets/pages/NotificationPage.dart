0efeca43f412443a29ca58a5112a0881af96bc3d
John <JohnPhamDeveloper@hotmail.com>
2019-08-05 18:26:11 -0700

    Initally load notifications on app start

--- lib/widgets/pages/NotificationPage.dart
+++ lib/widgets/pages/NotificationPage.dart
@@ -9,16 +9,6 @@ import 'dart:async';
 import 'dart:collection';
 import 'dart:math';
 
-//DEBUG
-//final colorsMe = [Colors.red, Colors.orange, Colors.pink, Colors.green];
-//final textMe = ["RED", "ORANGE", "PINK", "GREEN"];
-//final randomText = [
-//  "Yo my boi wheres my shoes",
-//  "I dunno i go tlost in the woods and found my way out the next morning",
-//  "ya babe",
-//  "YOu eat cheese"
-//];
-
 class NotificationPage extends StatefulWidget {
   final FirebaseUser firebaseUser;
 
@@ -30,23 +20,35 @@ class NotificationPage extends StatefulWidget {
 
 class _NotificationPageState extends State<NotificationPage> with AutomaticKeepAliveClientMixin {
   Timer timer;
-  //List<dynamic> notifications = List<dynamic>();
   Queue<Widget> notifications = Queue<Widget>();
-  bool toggleChildrenToRebuild = false;
-  bool recievedUsersInformation = false;
+  bool loadedPreviousNotifications = false; // First launch app so need to load previous 20 notifications from database
+//  bool toggleChildrenToRebuild = false;
+  // bool recievedUsersInformation = false;
 
   @override
   void initState() {
     super.initState();
+    // TODO First time loading this, push the 20 latest notification to the notifications queue
+    Firestore.instance.collection('private').document(widget.firebaseUser.uid).get().then((snapshot) {
+      final notificationLength = snapshot.data['notifications'].length - 1;
+      final start = notificationLength - 20;
+      var numberOfNotificationsToShow = 15;
+
+      // If the notifications isn't long as the number of notifications to show, then just
+      // show the notificationLength instead
+      if (notificationLength < numberOfNotificationsToShow) numberOfNotificationsToShow = notificationLength;
+
+      // Load the recent 15 notifications only
+      for (int i = start; i < start + numberOfNotificationsToShow; i++) {
+        _buildNotificationItem(snapshot.data['notifications'][i]);
+      }
+    });
 
     // Add a box at the end of the notifications so the last notification doesn't get blocked off
     notifications.add(SizedBox(height: 150));
 
-    // First time loading this, push the 20 latest notification to the notifications queue
-
-    // _auth = FirebaseAuth.instance;
-
-    //beginRebuildEverySeconds(60);
+    // Updates the time ago for each notification
+    _rebuildEverySeconds(60);
 
     // DEBUG AND FOR LEARNING (though make sure the user is signed in)
     //getData();
@@ -58,15 +60,13 @@ class _NotificationPageState extends State<NotificationPage> with AutomaticKeepA
   _listenToNotifications() {
     Firestore.instance.collection("private").document(widget.firebaseUser.uid).snapshots().listen((snapshot) {
       //if (!snapshot.exists) return Text("Nothing loaded...");
-
+      final lastItemIndex = snapshot.data['notifications'].length - 1;
       //  Take latest notification and add it to beginning of queue
-      _buildNotificationItem(snapshot);
+      _buildNotificationItem(snapshot.data['notifications'][lastItemIndex]);
     });
   }
 
-  // We need the bubble to update the notifications age every second with the given value
-  void beginRebuildEverySeconds(int seconds) {
-    // Just an empty setstate call to trigger a rebuild
+  void _rebuildEverySeconds(int seconds) {
     timer = Timer.periodic(
       Duration(seconds: seconds),
       (Timer t) => setState(
@@ -75,47 +75,47 @@ class _NotificationPageState extends State<NotificationPage> with AutomaticKeepA
     );
   }
 
-  void stopRebuildEverySeconds() {
+  void _stopRebuildEverySeconds() {
     timer.cancel();
   }
 
-  // DEBUG AND FOR LEARNING REFERENCE
-  void getData() async {
-    //print("Signed in...");
-    final QuerySnapshot querySnapshot =
-        await Firestore.instance.collection("users").where("name", isEqualTo: "Bobby Jones").getDocuments();
-    final List<DocumentSnapshot> documents = querySnapshot.documents;
-
-    final QuerySnapshot messageSnapshot =
-        await Firestore.instance.collection("users").document("jones").collection("messages").getDocuments();
-    final List<DocumentSnapshot> messageDocuments = messageSnapshot.documents;
-    //print(messageDocuments[0].data['timeSent'].seconds);
-
-    // Get when the message was sent
-    // TODO Change back
-    //int seconds = messageDocuments[0].data['timeSent'].seconds;
-    int seconds = 1;
-    //print(messageDocuments[1].data);
-
-    // This gets the correct time
-    var date = new DateTime.fromMillisecondsSinceEpoch(seconds * 1000);
-//      print(date);
-//      print(timeago.format(date));
-//      //  print(documents.length);
-//      print(documents[0].data);
-//      print("Name: ${documents[0].data['name']}");
-    // print("Registered: ${documents[0].data['registered']}");
-//      QuerySnapshot snapshot =
-//          await Firestore.instance.collection('users').getDocuments();
+//  // DEBUG AND FOR LEARNING REFERENCE (not used)
+//  void getData() async {
+//    //print("Signed in...");
+//    final QuerySnapshot querySnapshot =
+//        await Firestore.instance.collection("users").where("name", isEqualTo: "Bobby Jones").getDocuments();
+//    final List<DocumentSnapshot> documents = querySnapshot.documents;
 //
-//      for (DocumentSnapshot yes in snapshot.documents) {
-//        print(yes.data);
-//      }
-  }
+//    final QuerySnapshot messageSnapshot =
+//        await Firestore.instance.collection("users").document("jones").collection("messages").getDocuments();
+//    final List<DocumentSnapshot> messageDocuments = messageSnapshot.documents;
+//    //print(messageDocuments[0].data['timeSent'].seconds);
+//
+//    // Get when the message was sent
+//    // TODO Change back
+//    //int seconds = messageDocuments[0].data['timeSent'].seconds;
+//    int seconds = 1;
+//    //print(messageDocuments[1].data);
+//
+//    // This gets the correct time
+//    var date = new DateTime.fromMillisecondsSinceEpoch(seconds * 1000);
+////      print(date);
+////      print(timeago.format(date));
+////      //  print(documents.length);
+////      print(documents[0].data);
+////      print("Name: ${documents[0].data['name']}");
+//    // print("Registered: ${documents[0].data['registered']}");
+////      QuerySnapshot snapshot =
+////          await Firestore.instance.collection('users').getDocuments();
+////
+////      for (DocumentSnapshot yes in snapshot.documents) {
+////        print(yes.data);
+////      }
+//  }
 
   @override
   void dispose() {
-    stopRebuildEverySeconds();
+    _stopRebuildEverySeconds();
     super.dispose();
   }
 
@@ -123,19 +123,18 @@ class _NotificationPageState extends State<NotificationPage> with AutomaticKeepA
   // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
 
-  // Create a notification item with the data from the database
-  _buildNotificationItem(DocumentSnapshot documentSnapshot) {
+  // Create a notification widget with the data from the database
+  _buildNotificationItem(Map<dynamic, dynamic> data) {
     Widget item;
-    final snapshotLength = documentSnapshot.data['notifications'].length;
-    final lastItemIndex = snapshotLength - 1;
-    print(snapshotLength);
-    if (snapshotLength <= 0) return;
+    // final snapshotLength = data['notifications'].length;
+//    final lastItemIndex = snapshotLength - 1;
+//    print(snapshotLength);
+//    if (snapshotLength <= 0) return;
 
-    final timestamp = documentSnapshot.data['notifications'][lastItemIndex]['timeStamp'];
-    String message = documentSnapshot.data['notifications'][lastItemIndex]['message'];
-    String name = documentSnapshot.data['notifications'][lastItemIndex]['name'];
-    String uid = documentSnapshot.data['notifications'][lastItemIndex]['uid'];
-    //var date = new DateTime.fromMillisecondsSinceEpoch(seconds * 1000);
+    final timestamp = data['timeStamp'];
+    String message = data['message'];
+    String name = data['name'];
+    String uid = data['uid'];
     var date = new DateTime.fromMillisecondsSinceEpoch(timestamp.millisecondsSinceEpoch);
 
     print('$timestamp $message $name $date');
@@ -146,7 +145,7 @@ class _NotificationPageState extends State<NotificationPage> with AutomaticKeepA
     // User notification
     else {
       // This is allowed since it's public information.
-      // Client can modify it but won't really achieve anything other than getting public information
+      // Client can modify it, but won't really achieve anything other than getting public information
       Firestore.instance.collection('users').document(uid).get().then((snapshot) {
         String imageUrl = snapshot.data['photos'][0];
         print(imageUrl);
@@ -188,14 +187,6 @@ class _NotificationPageState extends State<NotificationPage> with AutomaticKeepA
       });
     }
   }
-
-  // Debug purposes
-//  void testSendNotification() {
-//    Firestore.instance.collection("users").document("jones").collection("messages").add({
-//      'message': "nonthing",
-//      'timeSent': DateTime.now(),
-//    });
-//  }
 
   @override
   Widget build(BuildContext context) {
