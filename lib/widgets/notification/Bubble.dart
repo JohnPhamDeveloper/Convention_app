a9b620d6b18f961feac5f3d3da361d27b2ec27f9
John <JohnPhamDeveloper@hotmail.com>
2019-08-08 20:18:16 -0700

    notification bubble

--- lib/widgets/notification/Bubble.dart
+++ lib/widgets/notification/Bubble.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cosplay_app/widgets/native_shapes/CircularBoxClipped.dart';
 import 'package:cosplay_app/constants/constants.dart';
+import 'package:cosplay_app/widgets/native_shapes/CircularBox.dart';
 
 class Bubble extends StatelessWidget {
   final String text;
+  final String name;
+  final String timeAgo;
 
-  Bubble({this.text});
+  Bubble({this.text, this.name, this.timeAgo});
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +17,49 @@ class Bubble extends StatelessWidget {
       bottomRight: Radius.circular(30.0),
       bottomLeft: Radius.circular(30.0),
       topLeft: Radius.circular(30.0),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 5.0, top: 5.0),
-        child: RichText(
-          text: TextSpan(
-            style: TextStyle(color: kBlack, fontWeight: FontWeight.w500, fontSize: 15.0),
-            children: [
-              TextSpan(text: text),
-            ],
+      child: Stack(
+        overflow: Overflow.visible,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0, top: 3.0),
+            child: RichText(
+              text: TextSpan(
+                style: TextStyle(color: kBlack, fontWeight: FontWeight.w500, fontSize: 15.0),
+                children: [
+                  TextSpan(text: text),
+                ],
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            top: -30,
+            left: 0,
+            child: CircularBox(
+              padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 10.0, right: 10.0),
+              maxWidth: 100,
+              radius: 20.0,
+              // Name bubble
+              child: Text(
+                name,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: kBlack, fontWeight: FontWeight.w600, fontSize: 14.0),
+              ),
+            ),
+          ),
+          Positioned(
+            top: -28,
+            left: 150,
+            child: CircularBox(
+              padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 10.0, right: 10.0),
+              radius: 20.0,
+              child: Text(
+                timeAgo,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: kBlack, fontWeight: FontWeight.w600, fontSize: 10.0),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/widgets/notification/NotificationItem.dart
+++ lib/widgets/notification/NotificationItem.dart
@@ -102,57 +102,20 @@ class _NotificationItemState extends State<NotificationItem> {
       },
       child: Padding(
         padding: const EdgeInsets.only(right: 20.0),
-        child: Stack(
-          children: <Widget>[
-            // Icon and Text
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 30.0,
-                left: 20.0,
-              ),
-              child: Row(
-                children: <Widget>[
-                  // Icon // Also where image should go
-                  _renderMiniUserOrIcon(),
-                  SizedBox(width: 15.0),
-                  // Message
-                  Expanded(
-                    child: Bubble(text: widget.message),
-                  ),
-                ],
-              ),
-            ),
-            // Person name on bubble
-            Positioned(
-              top: 22,
-              left: 100,
-              child: CircularBox(
-                padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 10.0, right: 10.0),
-                maxWidth: 100,
-                radius: 20.0,
-                // Name bubble
-                child: Text(
-                  widget.name,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: kBlack, fontWeight: FontWeight.w600, fontSize: 14.0),
-                ),
-              ),
-            ),
-            // Timestamp
-            Positioned(
-              top: 24,
-              left: 220,
-              child: CircularBox(
-                padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 10.0, right: 10.0),
-                radius: 20.0,
-                child: Text(
-                  newTimeAgo,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: kBlack, fontWeight: FontWeight.w600, fontSize: 10.0),
-                ),
-              ),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 30.0,
+            left: 20.0,
+          ),
+          child: Row(
+            children: <Widget>[
+              // Icon // Also where image should go
+              _renderMiniUserOrIcon(),
+              SizedBox(width: 15.0),
+              // Message
+              Expanded(child: Bubble(text: widget.message, name: widget.name, timeAgo: newTimeAgo)),
+            ],
+          ),
         ),
       ),
     );
